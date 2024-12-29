d93468e1c578a47f3cdf84344f88c72f88a2d23d
Luis Luna <33042685+luisluna-arg@users.noreply.github.com>
2024-12-29 05:35:03 -0300

    Receipts Fix

--- backend/Application/Queries/Receipts/GetReceiptsQuery.cs
+++ backend/Application/Queries/Receipts/GetReceiptsQuery.cs
@@ -1,15 +1,15 @@
 using SiradigCalc.Application.Queries.Records;
-using SiradigCalc.Core.Entities.Forms;
+using SiradigCalc.Core.Entities.Receipts;
 using SiradigCalc.Infra.Persistence.DbContexts;
 
 namespace SiradigCalc.Application.Queries.Receipts;
 
 public class GetReceiptsQuery()
-    : GetRecordInstancesQuery<Form>()
+    : GetRecordInstancesQuery<Receipt>()
 {
 }
 
 public class GetReceiptsQueryHandler(ISolutionDbContext dbContext)
-    : GetRecordInstancesQueryHandler<GetReceiptsQuery, Form, FormField, FormValue, FormTemplate, FormTemplateSection>(dbContext)
+    : GetRecordInstancesQueryHandler<GetReceiptsQuery, Receipt, ReceiptField, ReceiptValue, ReceiptTemplate, ReceiptTemplateSection>(dbContext)
 {
 }
