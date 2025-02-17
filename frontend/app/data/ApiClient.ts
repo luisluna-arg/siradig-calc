3425478eb5d46fd1aa57f106f84ddbd3795cf41d
Luis Luna <33042685+luisluna-arg@users.noreply.github.com>
2025-02-17 04:36:42 -0300

    Frontend: Updated APIClient

--- frontend/app/data/ApiClient.ts
+++ frontend/app/data/ApiClient.ts
@@ -1,61 +1,56 @@
-import { Template } from "./interfaces/Template";
-import { Record as DataRecord } from "./interfaces/Record";
-import axios from "axios";
-import { Agent } from "https";
-import { TemplateLinkReduced } from "./interfaces/TemplateLinkReduced";
+import axios, { AxiosInstance } from "axios";
+import { Template } from "@/data/interfaces/Template";
+import { Record as DataRecord } from "@/data/interfaces/Record";
+import { TemplateLinkReduced } from "@/data/interfaces/TemplateLinkReduced";
+import { RecordConversion } from "@/data/interfaces/RecordConversion";
+import { Catalog } from "./interfaces/Catalog";
 
 export class ApiClient {
   private baseURL: string;
-  private httpsAgent: Agent;
+  private client: AxiosInstance;
 
   constructor() {
-    this.baseURL = process.env.API_URL!;
+    this.baseURL =
+      typeof window === "undefined"
+        ? process.env.API_URL ?? ""
+        : import.meta.env.VITE_API_URL ?? "";
     if (!this.baseURL) {
       throw new Error("API_URL environment variable is not defined");
     }
-    const httpsAgent = new Agent({ rejectUnauthorized: false });
-    this.httpsAgent = httpsAgent;
-  }
 
-  private async request<T>(
-    endpoint: string,
-    options: RequestInit = {}
-  ): Promise<T> {
-    const url = `${this.baseURL}${endpoint}`;
-    const response = await fetch(url, options);
-
-    if (!response.ok) {
-      const errorText = await response.text();
-      throw new Error(
-        `Error: ${response.status} ${response.statusText} - ${errorText}`
-      );
-    }
-
-    return response.json() as Promise<T>;
-  }
-
-  public async get<T>(endpoint: string, headers: HeadersInit = {}): Promise<T> {
-    let result = await axios.get(`${this.baseURL}/${endpoint}`, {
-      httpsAgent: this.httpsAgent,
+    this.client = axios.create({
+      baseURL: this.baseURL,
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
+  }
 
+  private async getAxiosConfig() {
+    if (typeof window === "undefined") {
+      // Dynamically import https.Agent only on the server
+      const { Agent } = await import("https");
+      return {
+        httpsAgent: new Agent({ rejectUnauthorized: false }),
+      };
+    }
+    return {};
+  }
+
+  public async get<T>(endpoint: string): Promise<T> {
+    let result = await this.client.get(
+      `${this.baseURL}/${endpoint}`,
+      await this.getAxiosConfig()
+    );
     return result.data as T;
   }
 
-  // public async post<T, U>(
-  //   endpoint: string,
-  //   body: U,
-  //   headers: HeadersInit = {}
-  // ): Promise<T> {
-  //   return this.request<T>(endpoint, {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       ...headers,
-  //     },
-  //     body: JSON.stringify(body),
-  //   });
-  // }
+  public async delete(endpoint: string, id?: string): Promise<any> {
+    return await this.client.delete(
+      `${endpoint}` + (id ? `/${id}` : ''),
+      await this.getAxiosConfig()
+    );
+  }
 
   public async getTemplate(templateId: string): Promise<Template> {
     return await this.get<Template>(`api/records/templates/${templateId}`);
@@ -65,19 +60,42 @@ export class ApiClient {
     return await this.get<Template>(`api/records/templates`);
   }
 
+  public async getRecords(): Promise<DataRecord> {
+    return await this.get<DataRecord>(`api/records`);
+  }
+
   public async getRecord(recordId: string): Promise<DataRecord> {
     return await this.get<DataRecord>(`api/records/${recordId}`);
   }
 
-  public async getRecords(): Promise<DataRecord> {
-    return await this.get<DataRecord>(`api/records`);
+  public async deleteRecord(recordId: string): Promise<any> {
+    return await this.delete(`api/records`, recordId);
+  }
+
+  public async getConversions(): Promise<RecordConversion> {
+    return await this.get<RecordConversion>(`api/records/conversions`);
+  }
+
+  public async deleteConversion(sourceId: string, conversionId: string): Promise<RecordConversion> {
+    return await this.delete(`api/records/${sourceId}/conversions/${conversionId}`);
   }
 
   public async getLinks(): Promise<TemplateLinkReduced> {
     return await this.get<TemplateLinkReduced>(`api/records/templates/links`);
   }
 
-  public async getTemplateLink(leftTemplateId: string, rightTemplateId: string): Promise<TemplateLinkReduced> {
-    return await this.get<TemplateLinkReduced>(`api/records/templates/links/${leftTemplateId}/to/${rightTemplateId}`);
+  public async getTemplateLink(
+    leftTemplateId: string,
+    rightTemplateId: string
+  ): Promise<TemplateLinkReduced> {
+    return await this.get<TemplateLinkReduced>(
+      `api/records/templates/links/${leftTemplateId}/to/${rightTemplateId}`
+    );
   }
+
+  
+  public async getTemplateCatalog(): Promise<Catalog> {
+    return await this.get<Catalog>(`api/catalog/templates`);
+  }
+
 }
