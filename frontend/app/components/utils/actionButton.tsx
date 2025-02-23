023526920488909432e3350aa9caa9b6d280c2d8
Luis Luna <33042685+luisluna-arg@users.noreply.github.com>
2025-02-23 07:29:05 -0300

    Front: Added logic to actionButton and comboBox

--- frontend/app/components/utils/actionButton.tsx
+++ frontend/app/components/utils/actionButton.tsx
@@ -1,25 +1,30 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { CirclePlus, Trash2Icon } from "lucide-react";
+import { CirclePlus, Eraser, SendHorizonal, Trash2Icon } from "lucide-react";
 
-export type ActionButtonType = "add" | "delete";
+export type ActionButtonType = "add" | "delete" | "submit" | "clear";
 
 const icons: Record<ActionButtonType, JSX.Element> = {
   add: <CirclePlus />,
   delete: <Trash2Icon />,
+  submit: <SendHorizonal />,
+  clear: <Eraser />
 };
 
 const buttonClasses: Record<ActionButtonType, Array<string>> = {
-  add: ["hover:bg-green-400", "hover:text-white"],
-  delete: ["hover:bg-red-600", "hover:text-white"],
+  add: ["bg-green-100", "hover:bg-green-400", "hover:text-white"],
+  delete: ["bg-red-100", "hover:bg-red-600", "hover:text-white"],
+  submit: ["bg-green-100", "hover:bg-green-600", "hover:text-white"],
+  clear: ["bg-amber-100", "hover:bg-amber-400", "hover:text-white"],
 };
 
 export interface ActionButtonProps {
   type: ActionButtonType;
   onClick?: (e?: any) => Promise<void>;
+  text?: string;
 }
 
-const ActionButton = ({ type, onClick }: ActionButtonProps) => {
+const ActionButton = ({ type, text, onClick }: ActionButtonProps) => {
   const icon = icons[type];
 
   const classes = buttonClasses[type];
@@ -27,11 +32,12 @@ const ActionButton = ({ type, onClick }: ActionButtonProps) => {
   return (
     <Button
       variant={"outline"}
-      size="icon"
+      size={text ? undefined : "icon"}
       className={cn(classes)}
       onClick={onClick}
+      type={type === "submit" ? type : undefined}
     >
-      {icon}
+      {icon} {text ?? ""}
     </Button>
   );
 };
--- frontend/app/components/utils/comboBox.tsx
+++ frontend/app/components/utils/comboBox.tsx
@@ -20,61 +20,95 @@ import {
 } from "@/components/ui/popover";
 import { Catalog } from "@/data/interfaces/Catalog";
 
-export interface ComboboxDemoProps {
+export interface ComboBoxProps {
   data: Array<Catalog>;
+  value?: string;
+  name?: string;
   placeholder: string;
   searchPlaceholder: string;
+  onSelect?: ((data?: Catalog | null) => void) | undefined;
 }
 
-const ComboboxDemo = ({ data, placeholder = "Select...", searchPlaceholder = "Search..." }: ComboboxDemoProps) => {
+function findDataEntryByValue(data: Array<Catalog>, value?: string) {
+  return data.find((entry) => entry.id === value);
+}
+
+function findDataEnryByLabel(data: Array<Catalog>, label?: string) {
+  return data.find((entry) => entry.label === label);
+}
+
+const ComboBox = ({
+  data,
+  value,
+  name,
+  placeholder = "Select...",
+  searchPlaceholder = "Search...",
+  onSelect,
+}: ComboBoxProps) => {
   const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState("");
+  const [selectedValue, setSelectedValue] = React.useState(value);
+  const [comboLabel, setComboValue] = React.useState(
+    findDataEntryByValue(data, value)?.label ?? ""
+  );
+
+  function privateOnSelect(currentLabel: string) {
+    setComboValue(currentLabel === comboLabel ? "" : currentLabel);
+    const dataEntry = findDataEnryByLabel(data, currentLabel);
+
+    setOpen(false);
+
+    if (name) {
+      setSelectedValue(dataEntry?.id);
+    }
+
+    if (onSelect) {
+      onSelect(currentLabel === comboLabel ? null : dataEntry);
+    }
+  }
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-[200px] justify-between"
-        >
-          {value
-            ? data.find((entry) => entry.id === value)?.label
-            : placeholder}
-          <ChevronsUpDown className="opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
-        <Command>
-          <CommandInput placeholder={searchPlaceholder} className="h-9" />
-          <CommandList>
-            <CommandEmpty>No entry found.</CommandEmpty>
-            <CommandGroup>
-              {data.map((entry) => (
-                <CommandItem
-                  key={entry.id}
-                  value={entry.label}
-                  onSelect={(currentValue) => {
-                    setValue(currentValue === value ? "" : currentValue);
-                    setOpen(false);
-                  }}
-                >
-                  {entry.label}
-                  <Check
-                    className={cn(
-                      "ml-auto",
-                      value === entry.label ? "opacity-100" : "opacity-0"
-                    )}
-                  />
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+    <div>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-[200px] justify-between"
+          >
+            {findDataEnryByLabel(data, comboLabel)?.label ?? placeholder}
+            <ChevronsUpDown className="opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+          <Command>
+            <CommandInput placeholder={searchPlaceholder} className="h-9" />
+            <CommandList>
+              <CommandEmpty>No entry found.</CommandEmpty>
+              <CommandGroup>
+                {data.map((entry) => (
+                  <CommandItem
+                    key={entry.id}
+                    value={entry.label}
+                    onSelect={privateOnSelect}
+                  >
+                    {entry.label}
+                    <Check
+                      className={cn(
+                        "ml-auto",
+                        comboLabel === entry.label ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+      {name ? <input type="hidden" name={name} value={selectedValue} /> : <></>}
+    </div>
   );
 };
 
-export default ComboboxDemo;
+export default ComboBox;
