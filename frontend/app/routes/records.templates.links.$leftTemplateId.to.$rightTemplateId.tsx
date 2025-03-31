263b6caa855d0d0a22ba6730fee0e8921cfc2cf5
Luis Luna <33042685+luisluna-arg@users.noreply.github.com>
2025-03-31 02:02:32 -0300

    Frontend: Updated TemplateLink route

--- frontend/app/routes/records.templates.links.$leftTemplateId.to.$rightTemplateId.tsx
+++ frontend/app/routes/records.templates.links.$leftTemplateId.to.$rightTemplateId.tsx
@@ -1,14 +1,38 @@
-import { MetaFunction, type LoaderFunction } from "@remix-run/node";
-import TemplateLinkForm from "@/components/templateLinkForm";
+import {
+  LoaderFunctionArgs,
+  MetaFunction,
+  type LoaderFunction,
+} from "@remix-run/node";
+import { TemplateLinkForm } from "@/components/templateLinkForm";
 import { ApiClientProvider } from "@/data/ApiClientProvider";
 
-export const loader: LoaderFunction = async ({ params }) => {
+export const loader: LoaderFunction = async ({
+  params,
+  request,
+}: LoaderFunctionArgs) => {
   const { leftTemplateId, rightTemplateId } = params;
+
+  const searchParams = new URL(request.url).searchParams;
+
   let apiClient = new ApiClientProvider();
-  return await apiClient.TemplateLinks.getByIds(
-    leftTemplateId!,
-    rightTemplateId!
+
+  const queries = [];
+
+  const isEdit = searchParams.get("edit") ?? false;
+
+  queries.push(
+    apiClient.TemplateLinks.getByIds(leftTemplateId!, rightTemplateId!)
   );
+
+  queries.push(apiClient.Catalogs.getFieldTypes());
+
+  const [templateLink, fieldTypeCatalog] = await Promise.all(queries);
+
+  return {
+    templateLink,
+    isEdit,
+    fieldTypeCatalog,
+  };
 };
 
 export const meta: MetaFunction = () => {
