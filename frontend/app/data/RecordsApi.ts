4854a974ecfb44195d029756fbaac2a1471d97e1
Luis Luna <33042685+luisluna-arg@users.noreply.github.com>
2026-06-29 04:50:44 -0300

    Frontend: Add importCsv and importPdf to RecordsApi

--- frontend/app/data/RecordsApi.ts
+++ frontend/app/data/RecordsApi.ts
@@ -2,9 +2,34 @@ import { EntityApi } from "@/data/EntityApi";
 import { AxiosInstance } from "axios";
 import { RecordPostData } from "@/utils/route/interfaces/RecordPostData";
 import { Record as DataRecord } from "@/data/interfaces/Record";
+import { ImportResult } from "@/data/interfaces/ImportResult";
 
 export class RecordsApi extends EntityApi<DataRecord, RecordPostData> {
   constructor(client: AxiosInstance, baseUrl: string) {
     super(client, `${baseUrl}/records`);
+  }
+
+  public async importCsv(templateId: string, file: File): Promise<ImportResult> {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("templateId", templateId);
+    const result = await this.client.postForm(
+      `${this.baseURL}/import/csv`,
+      formData,
+      await this.getAxiosConfig()
+    );
+    return result.data as ImportResult;
+  }
+
+  public async importPdf(templateId: string, file: File): Promise<ImportResult> {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("templateId", templateId);
+    const result = await this.client.postForm(
+      `${this.baseURL}/import/pdf`,
+      formData,
+      await this.getAxiosConfig()
+    );
+    return result.data as ImportResult;
   }
 }
